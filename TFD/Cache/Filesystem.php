a91f413a1df0a998924d928047741ce4363ca25b
Pol Dellaiera <Pol@47194.no-reply.drupal.org>
2016-11-05 10:05:18 +0100

    Use even more stream wrappers functions.

--- TFD/Cache/Filesystem.php
+++ TFD/Cache/Filesystem.php
@@ -1,27 +1,39 @@
 <?php
 
 /**
- * @file
- * Class TFD_Cache_Filesystem
- * Part of the Drupal twig extension distribution.
- *
- * @see http://tfd7.rocks for more information
- * @author Davy Rolink
- * @author Rene Bakx
- */
-
-/**
- *
+ * Class TFD_Cache_Filesystem.
  */
 class TFD_Cache_Filesystem implements Twig_CacheInterface {
-  private $directory;
 
   /**
-   * @param $directory string The root cache directory
-   * @param $options   int    A set of options
+   * The scheme wrapper.
+   *
+   * @var DrupalLocalStreamWrapper
    */
-  public function __construct($directory) {
-    $this->directory = $directory;
+  private $scheme_wrapper;
+
+  /**
+   * Hold the original scheme wrapper string.
+   *
+   * @var string
+   */
+  private $wrapper;
+
+  /**
+   * Class constructor.
+   *
+   * @param string $cache_scheme
+   *   The cache scheme.
+   */
+  public function __construct($cache_scheme) {
+    if ($cache_scheme instanceof DrupalStreamWrapperInterface) {
+      $this->scheme_wrapper = $cache_scheme;
+    }
+    else {
+      $this->scheme_wrapper = file_stream_wrapper_get_instance_by_scheme(file_uri_scheme($cache_scheme));
+    }
+
+    $this->wrapper = $this->scheme_wrapper->getUri();
   }
 
   /**
@@ -38,7 +50,29 @@ class TFD_Cache_Filesystem implements Twig_CacheInterface {
   public function generateKey($name, $className) {
     $name = preg_replace("/(.*\\/themes\\/)/", "", $name);
     $hash = hash('sha256', $className);
-    return $this->directory . '/' . dirname($name) . '/' . basename($name) . '_' . $hash . '.php';;
+    return dirname($name) . '/' . basename($name) . '_' . $hash . '.php';
+  }
+
+  /**
+   * Support for fopen(), file_get_contents(), file_put_contents() etc.
+   *
+   * @param $uri
+   *   A string containing the URI to the file to open.
+   * @param $mode
+   *   The file mode ("r", "wb" etc.).
+   * @param $options
+   *   A bit mask of STREAM_USE_PATH and STREAM_REPORT_ERRORS.
+   * @param $opened_path
+   *   A string containing the path actually opened.
+   *
+   * @return
+   *   Returns TRUE if file was opened successfully.
+   *
+   * @see http://php.net/manual/streamwrapper.stream-open.php
+   */
+  public function open($uri, $mode, $options) {
+    $opened_path = NULL;
+    $this->scheme_wrapper->stream_open($uri, $mode, $options, $opened_path);
   }
 
   /**
@@ -48,16 +82,14 @@ class TFD_Cache_Filesystem implements Twig_CacheInterface {
    *   The cache key.
    * @param string $content
    *   The template representation as a PHP class.
+   *
+   * @return bool
+   *   True on success, otherwise False.
    */
   public function write($key, $content) {
-    $directory = dirname($key);
-    if ($this->ensureDirectory($directory)) {
-      $htaccess_path = $directory . '/.htaccess';
-      if (!file_exists($htaccess_path) && file_put_contents($htaccess_path, static::htaccessLines())) {
-        @chmod($htaccess_path, 0444);
-      }
-    }
-    return (bool) file_put_contents($key, $content);
+    $this->createDirectory($key);
+    $this->open($this->wrapper . $key, 'w', array());
+    return $this->scheme_wrapper->stream_write($content);
   }
 
   /**
@@ -65,11 +97,16 @@ class TFD_Cache_Filesystem implements Twig_CacheInterface {
    *
    * @param string $key
    *   The cache key.
+   *
+   * @return bool
+   *   True on success, otherwise False.
    */
   public function load($key) {
+    $key = $this->wrapper . $key;
+
     // The FALSE returned on failure is enough for the caller to handle this,
     // we do not want a warning too.
-    return (@include_once $key) !== FALSE;
+    return (include_once $key) !== FALSE;
   }
 
   /**
@@ -81,6 +118,8 @@ class TFD_Cache_Filesystem implements Twig_CacheInterface {
    * @return int
    */
   public function getTimestamp($key) {
+    $key = $this->wrapper . $key;
+
     if (!file_exists($key)) {
       return 0;
     }
@@ -92,7 +131,7 @@ class TFD_Cache_Filesystem implements Twig_CacheInterface {
    * Removes all files in this bin.
    */
   public function deleteAll() {
-    return $this->unlink($this->directory);
+    return $this->unlink($this->scheme_wrapper->getDirectoryPath());
   }
 
   /**
@@ -130,81 +169,6 @@ class TFD_Cache_Filesystem implements Twig_CacheInterface {
   }
 
   /**
-   * Returns the standard .htaccess lines that Drupal writes to file directories.
-   *
-   * @param bool $private
-   *   (optional) Set to FALSE to return the .htaccess lines for an open and
-   *   public directory. The default is TRUE, which returns the .htaccess lines
-   *   for a private and protected directory.
-   *
-   * @return string
-   *   The desired contents of the .htaccess file.
-   *
-   * @see file_create_htaccess()
-   */
-  public static function htaccessLines($private = TRUE) {
-    $lines = <<<EOF
-# Turn off all options we don't need.
-Options -Indexes -ExecCGI -Includes -MultiViews
-
-# Set the catch-all handler to prevent scripts from being executed.
-SetHandler Drupal_Security_Do_Not_Remove_See_SA_2006_006
-<Files *>
-  # Override the handler again if we're run later in the evaluation list.
-  SetHandler Drupal_Security_Do_Not_Remove_See_SA_2013_003
-</Files>
-
-# If we know how to do it safely, disable the PHP engine entirely.
-<IfModule mod_php5.c>
-  php_flag engine off
-</IfModule>
-EOF;
-
-    if ($private) {
-      $lines = <<<EOF
-# Deny all requests from Apache 2.4+.
-<IfModule mod_authz_core.c>
-  Require all denied
-</IfModule>
-
-# Deny all requests from Apache 2.0-2.2.
-<IfModule !mod_authz_core.c>
-  Deny from all
-</IfModule>
-$lines
-EOF;
-    }
-
-    return $lines;
-  }
-
-  /**
-   * Ensures the directory exists, has the right permissions, and a .htaccess.
-   *
-   * For compatibility with open_basedir, the requested directory is created
-   * using a recursion logic that is based on the relative directory path/tree:
-   * It works from the end of the path recursively back towards the root
-   * directory, until an existing parent directory is found. From there, the
-   * subdirectories are created.
-   *
-   * @param string $directory
-   *   The directory path.
-   * @param int $mode
-   *   The mode, permissions, the directory should have.
-   *
-   * @return bool
-   *   TRUE if the directory exists or has been created, FALSE otherwise.
-   */
-  protected function ensureDirectory($directory, $mode = 0777) {
-    if ($this->createDirectory($directory, $mode)) {
-      $htaccess_path = $directory . '/.htaccess';
-      if (!file_exists($htaccess_path) && file_put_contents($htaccess_path, static::htaccessLines())) {
-        @chmod($htaccess_path, 0444);
-      }
-    }
-  }
-
-  /**
    * Ensures the requested directory exists and has the right permissions.
    *
    * For compatibility with open_basedir, the requested directory is created
@@ -213,38 +177,17 @@ EOF;
    * directory, until an existing parent directory is found. From there, the
    * subdirectories are created.
    *
-   * @param string $directory
-   *   The directory path.
+   * @param string $key
+   *   The cache key.
    * @param int $mode
    *   The mode, permissions, the directory should have.
-   * @param bool $is_backwards_recursive
-   *   Internal use only.
    *
    * @return bool
    *   TRUE if the directory exists or has been created, FALSE otherwise.
    */
-  protected function createDirectory($directory, $mode = 0777, $is_backwards_recursive = FALSE) {
-    // If the directory exists already, there's nothing to do.
-    if (is_dir($directory)) {
-      return TRUE;
-    }
-    // Otherwise, try to create the directory and ensure to set its permissions,
-    // because mkdir() obeys the umask of the current process.
-    if (is_dir($parent = dirname($directory))) {
-      // If the parent directory exists, then the backwards recursion must end,
-      // regardless of whether the subdirectory could be created.
-      if ($status = mkdir($directory)) {
-        // Only try to chmod() if the subdirectory could be created.
-        $status = chmod($directory, $mode);
-      }
-      return $is_backwards_recursive ? TRUE : $status;
-    }
-    // If the parent directory and the requested directory does not exist and
-    // could not be created above, walk the requested directory path back up
-    // until an existing directory is hit, and from there, recursively create
-    // the sub-directories. Only if that recursion succeeds, create the final,
-    // originally requested subdirectory.
-    return $this->createDirectory($parent, $mode, TRUE) && mkdir($directory) && chmod($directory, $mode);
+  protected function createDirectory($key, $mode = 0777) {
+    $options = array(STREAM_MKDIR_RECURSIVE);
+    $this->scheme_wrapper->mkdir($this->wrapper . dirname($key), $mode, $options);
   }
 
 }
